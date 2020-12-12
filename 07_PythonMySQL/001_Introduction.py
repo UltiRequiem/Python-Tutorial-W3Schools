28df4beaf2442b25e850dd8b100d8f9d1df79cde
Zero Requiem <eliaz.bobadilladev@gmail.com>
2020-12-12 13:24:36 -0500

    Python MySQL

---
+++ 07_PythonMySQL/001_Introduction.py
@@ -0,0 +1,31 @@
+#Python MySQL
+
+'''
+Python can be used in database applications.
+
+One of the most popular databases is MySQL.
+'''
+
+'''
+MySQL Database
+To be able to experiment with the code examples in this tutorial, you should have MySQL installed on your computer.
+
+You can download a free MySQL database at https://www.mysql.com/downloads/.
+'''
+
+'''
+Install MySQL Driver
+Python needs a MySQL driver to access the MySQL database.
+
+In this tutorial we will use the driver "MySQL Connector".
+
+We recommend that you use PIP to install "MySQL Connector".
+
+PIP is most likely already installed in your Python environment.
+
+Navigate your command line to the location of PIP, and type the following:
+
+python -m pip install mysql-connector-python
+
+'''
+#Now you have downloaded and installed a MySQL driver!
