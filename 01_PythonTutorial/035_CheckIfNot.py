68bc329253b131e176e2fa619dac2c50867b4a77
Zero Requiem <eliaz.bobadilladev@gmail.com>
2020-12-12 18:33:08 -0500

    Commit owO

---
+++ 01_PythonTutorial/035_CheckIfNot.py
@@ -0,0 +1,15 @@
+#Check if NOT
+
+txt = "Hello buddie unu!"
+print("owo" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("Yes, 'expensive' is NOT present.")
+
+'''
+Terminal:
+True
+Yes, 'expensive' is NOT present.
+'''
+#https://www.w3schools.com/python/python_strings.asp
