ba889072cba3d1ac6eebef187b910b30fc37f911
Zero Requiem <eliaz.bobadilladev@gmail.com>
2020-12-12 13:40:07 -0500

    References

---
+++ 09_PythonReference/001_Overview.py
@@ -0,0 +1,38 @@
+#Python Reference
+
+'''
+Built-in Functions https://www.w3schools.com/python/python_ref_functions.asp
+
+String Mehods https://www.w3schools.com/python/python_ref_string.asp
+
+List Methods https://www.w3schools.com/python/python_ref_list.asp
+
+Dictionary Methods https://www.w3schools.com/python/python_ref_dictionary.asp
+
+Tuple Methods https://www.w3schools.com/python/python_ref_tuple.asp
+
+Set Methods https://www.w3schools.com/python/python_ref_set.asp
+
+File Methods https://www.w3schools.com/python/python_ref_file.asp
+
+Keywords https://www.w3schools.com/python/python_ref_keywords.asp
+
+Exceptions https://www.w3schools.com/python/python_ref_exceptions.asp
+
+Glosary https://www.w3schools.com/python/python_ref_glossary.asp
+
+'''
+
+#Module Reference
+
+'''
+Random Module https://www.w3schools.com/python/module_random.asp
+
+Request Module https://www.w3schools.com/python/module_requests.asp
+
+Math Module https://www.w3schools.com/python/module_requests.asp
+
+CMath Module https://www.w3schools.com/python/module_cmath.asp
+'''
+
+#https://www.w3schools.com/python/python_reference.asp
