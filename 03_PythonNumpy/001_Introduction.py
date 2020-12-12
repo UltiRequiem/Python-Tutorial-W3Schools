c545da8719f5773499e9c182ffb2fe5d467bdb18
Zero Requiem <eliaz.bobadilladev@gmail.com>
2020-12-12 13:11:45 -0500

    Python Numpy

---
+++ 03_PythonNumpy/001_Introduction.py
@@ -0,0 +1,43 @@
+'''
+NumPy Introduction
+
+What is NumPy?
+NumPy is a Python library used for working with arrays.
+
+It also has functions for working in domain of linear algebra, fourier transform, and matrices.
+
+NumPy was created in 2005 by Travis Oliphant. It is an open source project and you can use it freely.
+
+NumPy stands for Numerical Python.
+
+Why Use NumPy?
+In Python we have lists that serve the purpose of arrays, but they are slow to process.
+
+NumPy aims to provide an array object that is up to 50x faster than traditional Python lists.
+
+The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
+
+Arrays are very frequently used in data science, where speed and resources are very important.
+
+Data Science: is a branch of computer science where we study how to store, use and analyze data for deriving information from it.
+
+'''
+
+'''
+
+Why is NumPy Faster Than Lists?
+NumPy arrays are stored at one continuous place in memory unlike lists, so processes can access and manipulate them very efficiently.
+
+This behavior is called locality of reference in computer science.
+
+This is the main reason why NumPy is faster than lists. Also it is optimized to work with latest CPU architectures.
+
+Which Language is NumPy written in?
+NumPy is a Python library and is written partially in Python, but most of the parts that require fast computation are written in C or C++.
+
+Where is the NumPy Codebase?
+The source code for NumPy is located at this github repository https://github.com/numpy/numpy
+
+'''
+
+#github: enables many people to work on the same codebase.
