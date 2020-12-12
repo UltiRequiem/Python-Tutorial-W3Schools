397325ed492a88a23544dac8c909e5a7fb5c31ec
Zero Requiem <eliaz.bobadilladev@gmail.com>
2020-12-12 18:18:37 -0500

    unu

---
+++ 01_PythonTutorial/033_StingLength.py
@@ -0,0 +1,10 @@
+#String Length
+
+a = "Good Morning owO!"
+print(len(a))
+
+'''
+Terminal:
+17
+'''
+#https://www.w3schools.com/python/python_strings.asp
---
+++ 01_PythonTutorial/034_CheckString.py
@@ -0,0 +1,20 @@
+#Check String
+'''To check if a certain phrase or character is present in a string, we can use the keyword in.'''
+
+#Check if "free" is present in the following text:
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "Hello buddie owo!"
+if "owo" in txt:
+  print("Yes, 'owo' is present.")
+
+'''
+Terminal:
+True
+Yes, 'owo' is present.
+'''
+
+#Learn more about If statements in our Python If...Else chapter: https://www.w3schools.com/python/python_conditions.asp
+#https://www.w3schools.com/python/python_strings.asp
