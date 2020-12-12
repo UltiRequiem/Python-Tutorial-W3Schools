5ed74f0cdd3d32765bcf2978c292d63f1530e4de
Zero Requiem <eliaz.bobadilladev@gmail.com>
2020-12-12 13:28:07 -0500

    PythonMondongo

---
+++ 08_PythonMongoDB/001_Introduction.py
@@ -0,0 +1,34 @@
+#Python MongoDB
+'''
+Python can be used in database applications.
+
+One of the most popular NoSQL database is MongoDB.
+'''
+
+'''
+MongoDB
+MongoDB stores data in JSON-like documents, which makes the database very flexible and scalable.
+
+To be able to experiment with the code examples in this tutorial, you will need access to a MongoDB database.
+
+You can download a free MongoDB database at https://www.mongodb.com.
+
+Or get started right away with a MongoDB cloud service at https://www.mongodb.com/cloud/atlas.
+'''
+
+'''
+PyMongo
+Python needs a MongoDB driver to access the MongoDB database.
+
+In this tutorial we will use the MongoDB driver "PyMongo".
+
+We recommend that you use PIP to install "PyMongo".
+
+PIP is most likely already installed in your Python environment.
+
+Navigate your command line to the location of PIP, and type the following:
+
+python -m pip install pymongo
+'''
+#Now you have downloaded and installed a mongoDB driver.
+
